0d553d94481a0b80ff8e8dd45a0366a03b5987d1
Prince gupta <pg0799231@gmail.com>
2025-11-12 17:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-// complete this js code
-function Person(name, age) {
-	constructor(name, age){
-	this.name = name;
-	this.ager = age;
-	}
-	greet(){
-	 return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
-	}
+// Person class
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+
+  greet() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
 }
+
+
 class Employee extends Person {
   constructor(name, age, jobTitle) {
     super(name, age);
     this.jobTitle = jobTitle;
   }
-jobGreet() {
-    return `Hello, my name is ${this.name}, I am ${this.age} years old, and I work as a ${this.jobTitle}.`;
+
+  
+  jobGreet() {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
   }
 }
 
-// Do not change code below this line
+// ✅ Expose to window (important for Cypress)
 window.Person = Person;
 window.Employee = Employee;
