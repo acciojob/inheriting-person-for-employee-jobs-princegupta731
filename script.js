a52a3ef3229a2b0d717132781b193366c6728b1f
Prince gupta <pg0799231@gmail.com>
2025-11-12 17:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // complete this js code
-function Person(name, age) {}
-
-function Employee(name, age, jobTitle) {}
+function Person(name, age) {
+	constructor(name, age){
+	this.name = name;
+	this.ager = age;
+	}
+	greet(){
+	 return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+	}
+}
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
+jobGreet() {
+    return `Hello, my name is ${this.name}, I am ${this.age} years old, and I work as a ${this.jobTitle}.`;
+  }
+}
 
 // Do not change code below this line
 window.Person = Person;
